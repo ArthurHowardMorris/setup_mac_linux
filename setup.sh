422b289bb7121f704ff6604c2ad4fda21a3f3e99
Arthur Morris <53531149+ArthurHowardMorris@users.noreply.github.com>
2023-04-19 18:28:24 +0800

    added git and python updates

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 echo "checking system and prepping package manager"
 echo "system is $(uname)"
-# if this is a mac then check for homebrew and install or update
+
+## Step 1 Install `homebrew` (checks whether this is mac or linux)
+
 if [  "$(uname)" == "Darwin" ]
 then
     if ! command -v brew &> /dev/null
@@ -33,9 +35,13 @@ fi
 echo "check for updates"
 brew update
 
+## Step 2 install all the formulae in formulae.txt which is populated from `brew list`
 echo "install formulae from formulae.txt"
 xargs brew  install < formulae.txt
 
+
+## Step 3 install mac apps an casks (if this is a mac)
+## NB: that this includes miniconda
 if [  "$(uname)" == "Darwin" ]
 then
     echo "installing mac apps and casks"
@@ -44,7 +50,8 @@ then
     conda init "$(basename "${SHELL}")"
 fi
 
-
+## Step 3 cont'd install flatpaks and miniconda if this is
+## linux.
 if [ "$(uname)" == "Linux" ]
 then
 	flatpak install https://downloads.1password.com/linux/flatpak/1Password.flatpakref
@@ -61,15 +68,6 @@ then
 	sudo apt-get latexmk
 fi
 
-echo "now setting up an anaconda stack with python 3.10"
-conda create --name 3point10 python=3.10 anaconda pyarrow pip ipykernel notebook  nb_conda_kernels 
-conda activate 3point10
-conda install -c conda-forge jupyter_contrib_nbextensions rise plotnine stata_kernel 
-python -m ipykernel install --user --name 3point10
-jupyter contrib nbextension install --user
-#python -m stata_kernel.install
-pip install wrds
-pip install mapply
 
 # TODO: set  this up so it works alittle more of at all on both:
 # make an ssh key for the main github account, without passphrase, in the default location
@@ -78,8 +76,27 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 # https://ostechnix.com/how-to-use-pbcopy-and-pbpaste-commands-on-linux/
 cat ~/.ssh/id_rsa.pub | xclip -selection c
+read -p "Public key is now in the system clipboard. Go to https://github.com/settings/keys to add this to github. Ready to continue (y/n)?" choice
+case "$choice" in 
+  y|Y ) echo "yes";;
+  n|N ) echo "no";;
+  * ) echo "invalid";;
+esac
 # now the public key is in the clipboard and you can add it to github under settings
 # add a pause here and a prompt to load into git hub.
 git config --global user.email  "53531149+ArthurHowardMorris@users.noreply.github.com"
 git config --global user.name "ArthurHowardMorris"
 
+## Step 5: use conda to set up the anaconda stack with the new hotness (python 11)
+## Note that this might benefit from being it's own script as it will probably fail unless you restart the terminal
+echo "reloading zsh so we can use conda"
+exec zsh -l
+echo "now setting up an anaconda stack with python 3.11"
+conda create --name 3point11 python=3.11 anaconda pyarrow pip ipykernel notebook  nb_conda_kernels 
+conda activate 3point11
+conda install -c conda-forge jupyter_contrib_nbextensions rise plotnine stata_kernel 
+python -m ipykernel install --user --name 3point11
+jupyter contrib nbextension install --user
+#python -m stata_kernel.install
+pip install wrds
+pip install mapply
