034141b5d419e0df1033c2f33406e5b4260a40c1
ArthurHowardMorris <53531149+ArthurHowardMorris@users.noreply.github.com>
2023-05-12 14:22:28 +0800

    conda is all set up and most of what we need for stata is also ready

---
+++ conda_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "first setting up an anaconda stack with 3.10"
+
+conda create --name 3point10 python=3.10 anaconda pyarrow pip ipykernel notebook  nb_conda_kernels rise
+conda activate 3point10
+conda install -c conda-forge jupyter_contrib_nbextensions plotnine stata_kernel 
+python -m ipykernel install --user --name 3point10
+jupyter contrib nbextension install --user
+# python -m stata_kernel.install
+pip install wrds
+conda deactivate
+
+echo "now setting up an anaconda stack with python 3.11"
+conda create --name 3point11 python=3.11 anaconda pyarrow pip ipykernel notebook  nb_conda_kernels 
+conda activate 3point11
+conda install -c conda-forge plotnine 
+python -m ipykernel install --user --name 3point11
+jupyter contrib nbextension install --user
+# python -m stata_kernel.install
+pip install wrds
+
--- setup_darwin.sh
+++ setup_darwin.sh
@@ -62,15 +62,10 @@ git config --global user.email  "53531149+ArthurHowardMorris@users.noreply.githu
 git config --global user.name "ArthurHowardMorris"
 
 ## Step 5 use conda to set up the anaconda stack (at py 11 now)
-conda init "$(basename "{SHELL}")"
-echo "reloading zsh so we can use conda"
+conda init "$(basename "$SHELL")"
+echo "****************************************"
+echo "* reloading zsh so we can use conda    *"
+echo "* now check the install to this point  *"
+echo "* and run the python env setup script  *"
+echo "****************************************"
 exec zsh -l
-echo "now setting up an anaconda stack with python 3.11"
-conda create --name 3point11 python=3.11 anaconda pyarrow pip ipykernel notebook  nb_conda_kernels 
-conda activate 3point11
-conda install -c conda-forge jupyter_contrib_nbextensions rise plotnine stata_kernel 
-python -m ipykernel install --user --name 3point11
-jupyter contrib nbextension install --user
-#python -m stata_kernel.install
-pip install wrds
-pip install mapply
